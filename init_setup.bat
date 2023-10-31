5385a0f7f498ecbd1725133490f207af86a091b6
Moses <moseaiml@gmail.com>
2023-10-31 21:09:32 +0300

    init_set renamed to .bat and requirements

---
+++ init_setup.bat
@@ -0,0 +1,20 @@
+@echo off
+
+echo [%date% %time%]: "START"
+
+echo [%date% %time%]: "creating env with python 3.8 version"
+
+#conda create --prefix ./env python=3.8 -y
+
+echo [%date% %time%]: "activating the environment"
+
+#conda activate ./env
+
+echo [%date% %time%]: "installing the dev requirements"
+
+pip install -r requirements.txt
+
+echo [%date% %time%]: "END"
+
+
+echo [$(date)]: "END" 
